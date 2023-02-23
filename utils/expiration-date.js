af6eb7d79aa0d895410bf82b031904d714f75128
TheMike1972 <TheMike1972@github.com>
2023-02-23 12:39:07 +0100

    logic done

--- utils/expiration-date.js
+++ utils/expiration-date.js
@@ -1,23 +1,17 @@
 const Warranty = require("../models/Warranty.model")
 const Item = require("../models/Item.model")
 
-
 async function getExpiratonDate(item) {
     try {
         if (!item._id) {
             return null
         }
-
         const productWarranties = await Warranty.find({ product: item._id }, { durationInMonths: 1 })
-
         const warrantyStart = item.purchaseDate
-
         const totalWarrantyInMonth = productWarranties.reduce((acc, warranty) => {
             return acc + warranty.durationInMonths;
         }, 0);
-
         const expirationDate = new Date(new Date(warrantyStart).setMonth(warrantyStart.getMonth() + totalWarrantyInMonth))
-
         return expirationDate
     } catch (error) {
         console.error(error)
