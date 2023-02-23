a6f103c00d06dc42fd4523d88ee1b350f16c7b67
TheMike1972 <TheMike1972@github.com>
2023-02-23 09:19:51 +0100

    update

--- public/js/expiration-date.js
+++ public/js/expiration-date.js
@@ -9,9 +9,10 @@ async function warrantyExpirationDate(productId) {
 
         const warrantyStart = new Date(item.purchaseDate)
         // const itemWarranty = await Item.
-        // const warrantyDurationInMonth = await Warranty.find()
+        const warrantyDurationInMonth = await Warranty.find()
         // (là il faut récupérer toutes les warranties qui ont le même item en ID et les additioner)
         const expirationDate = warrantyStart.setMonth(warrantyStart.getMonth() + warrantyDurationInMonth)
+        return expirationDate
     } catch (error) {
         next(error)
     }
